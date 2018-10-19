a819231127b211bf0fa024e60e98aed04aa52c1a
admin <3903927@qq.com>
2018-10-19 15:46:04 +0800

    Add AirHockey1 template

---
+++ AirHockey1/app/src/main/java/om/tory/airhockey1/MainActivity.java
@@ -0,0 +1,34 @@
+package om.tory.airhockey1;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGlSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGlSurfaceView = findViewById(R.id.glsvMain);
+        mGlSurfaceView.setEGLContextClientVersion(2);
+        mGlSurfaceView.setEGLConfigChooser(8,8,8,8,16,0);//tory ?
+        mGlSurfaceView.setRenderer(new MyRenderer());
+        mGlSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGlSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGlSurfaceView.onResume();
+    }
+}
